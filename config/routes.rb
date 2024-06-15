06bf3c87e74fed210ee8d0db66f842f15299f698
phadhiil001 <phadhiil001@gmail.com>
2024-06-15 09:40:21 -0500

    Finished Part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "products#index"
 
-  get "/products", to: "products#index"
+  # get "/products", to: "products#index"
+  # get "/products/:id", to: "products#show"
+
+  resources :products
 end
 
 Rails.application.routes.draw do
