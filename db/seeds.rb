c1cdb71a5ef1a2489606814d56fc849964b9bf68
phadhiil001 <phadhiil001@gmail.com>
2024-06-15 09:24:13 -0500

    5th trial

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,6 @@ require 'faker'
 676.times do
     Product.create(
     title: Faker::Commerce.product_name,
-    description: Faker::Lorem.paragraph,
     price: Faker::Commerce.price(range: 0.1..100.0),
     stock_quantity: Faker::Number.between(from: 1, to: 100)
     )
